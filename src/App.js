82ab9bf7758daca98b211ba1cde6eefdb1f3f8ec
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-25 18:12:42 -0800

    exports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 import { Navbar, Infobar } from './components';
 import { Achievements, Experience, Projects, Thanks } from "./containers";
 
 const App = () => {
     return (
-        <div className="App">
-            <Achievements/>
+        <div className="App">            <Achievements/>
             <Experience/>
             <Projects/>
             <Thanks/>
         </div>
     )
 }
+
+export default App
