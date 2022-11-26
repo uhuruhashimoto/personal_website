22c718f75fb64e3e0aba3c12e11a230c6f225753
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-25 17:53:55 -0800

    linking and laying out blocks; subject to change

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import { Navbar, Infobar } from './components';
+import { Achievements, Experience, Projects, Thanks } from "./containers";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Achievements/>
+            <Experience/>
+            <Projects/>
+            <Thanks/>
+        </div>
+    )
+}
---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Achievements } from './achievements/Achievements'
+export { default as Experience } from './experience/Experience'
+export { default as Projects } from './projects/Projects'
+export { default as Thanks } from './thanks/Thanks'
