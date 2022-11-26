3466fe0633ee18d4fab6a330adadc43b9bb89c5d
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-26 12:40:42 -0800

    a better lookin homepage :)

--- src/containers/bio/Bio.jsx
+++ src/containers/bio/Bio.jsx
@@ -4,13 +4,6 @@ import { Infobar, Navbar } from '../../components';
 
 const Bio = () => {
   return (
-    // <body>
-    //   <div classname='website__bio'>
-    //     <div className='website__bio-name'>
-    //       <h1>Uhuru Hashimoto</h1>
-    //     </div>
-    //   </div>
-    // </body>
     <div className='website__bio'>
       <div className='website__bio-name'>
         Uhuru Hashimoto
