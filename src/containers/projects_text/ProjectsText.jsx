6990cd426a78339746b824352bdd86fd7bc4c9b9
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-26 19:20:08 -0800

    project line breaks and bold headers

--- src/containers/projects_text/ProjectsText.jsx
+++ src/containers/projects_text/ProjectsText.jsx
@@ -7,52 +7,52 @@ const ProjectsText = () => {
     <link rel="stylesheet" href="css/animate.css"></link>
     <div className='website__projectstext-description'>
         <p1>
-            Software Development
+            Software Development<br></br>
         </p1>
         <p2>
-            AR Gift App on Android. User login, verification, and virtual token system. Unity integration and scripting. Java. 21 Winter
+            AR Gift App on Android. User login, verification, and virtual token system. Unity integration and scripting. Java. 21 Winter<br></br>
         </p2>
         <p3>
-            Blockchain Playlist Voting System. Song queue selected by blockchain verification of voters. Mining incentivized. Python. 21 Spring
+            Blockchain Playlist Voting System. Song queue selected by blockchain verification of voters. Mining incentivized. Python. 21 Spring<br></br>
         </p3>
         <p4>
-            ML modeling on MNIST dataset; achieved 96% accuracy. Python. 21 Spring.
+            ML modeling on MNIST dataset; achieved 96% accuracy. Python. 21 Spring<br></br>
         </p4>
         <p5>
-            Search Engine. Query parsing, recursive web crawling, and result ranking algorithm. C 21 Summer
+            Search Engine. Query parsing, recursive web crawling, and result ranking algorithm. C 21 Summer<br></br>
         </p5>
         <p6>
-            Sudoku Solver. Recursive DFS with some branch optimization. C 21 Summer
+            Sudoku Solver. Recursive DFS with some branch optimization. C 21 Summer<br></br>
         </p6>
         <p7>
-            Computer Architecture; built 'Y86' X86 simulated architecture from transistor level up. Runs on abbreviated X86 ISA. Includes mmapped terminal and read/write functionality. Run hello world or simple programs (ported to hex)! Spring 2022
+            Computer Architecture; built 'Y86' X86 simulated architecture from transistor level up. Runs on abbreviated X86 ISA. Includes mmapped terminal and read/write functionality. Run hello world or simple programs (ported to hex)! Spring 2022<br></br>
         </p7>
         <p8>
-            Operating Systems: wrote 'Yalnix' monolithic, preemptive, ring-0-only kernel with double address space to run on Ubuntu (with configured signal handlers). Fall 2022
+            Operating Systems: wrote 'Yalnix' monolithic, preemptive, ring-0-only kernel with double address space to run on Ubuntu (with configured signal handlers). Fall 2022<br></br>
         </p8>
         <p9>
-            Embedded Systems Development
+            Embedded Systems Development<br></br>
         </p9>
         <p10>
-            Wordle on FPGA. On-chip resource allocation and state machine functionality for integrated circuit design. 100 MHz clock speed. Runs on full Wordle dictionary. Xilinx Vivido, VHDL. Spring 2022
+            Wordle on FPGA. On-chip resource allocation and state machine functionality for integrated circuit design. 100 MHz clock speed. Runs on full Wordle dictionary. Xilinx Vivido, VHDL. Spring 2022<br></br>
         </p10>
         <p11>
-            Meshtastic: implemented dataflow and backend for mesh network for use in emergency response, including mesh network algorithm optimization and dataflow. TS, C++, Rust. 22 Fall; development ongoing
+            Meshtastic: implemented dataflow and backend for mesh network for use in emergency response, including mesh network algorithm optimization and dataflow. TS, C++, Rust. 22 Fall; development ongoing<br></br>
         </p11>
         <p12>
-            Arduino bare-metal code projects (pwm driver, register signal handlers, network FSM). 2022 Winter Spring 
+            Arduino bare-metal code projects (pwm driver, register signal handlers, network FSM). 2022 Winter Spring<br></br> 
         </p12>
         <p13>
-            Power Electronics
+            Power Electronics<br></br>
         </p13>
         <p14>
-            PD Control of Duck Car and accompanying Frequency Domain Analysis. Hardware tested. 21 Fall
+            PD Control of Duck Car and accompanying Frequency Domain Analysis. Hardware tested. 21 Fall<br></br>
         </p14>
         <p15>
-            Breadboard Heart Rate Monitor - electret microphone signal processing, filtering, Class D amplification on breadboards. User tested. 22 Winter
+            Breadboard Heart Rate Monitor - electret microphone signal processing, filtering, Class D amplification on breadboards. User tested. 22 Winter<br></br>
         </p15>
         <p16>
-            High-Power Rectifier, using Discontinuous Indirect Converter and inductor built to specifications. Graduate Power Electronics. 22 Fall
+            High-Power Rectifier, using Discontinuous Indirect Converter and inductor built to specifications. Graduate Power Electronics. 22 Fall<br></br>
         </p16>
     </div>
 </div>
