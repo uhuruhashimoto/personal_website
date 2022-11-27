fb1670e77c9781dabe04711365b49aad5ae88089
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-27 10:54:06 -0800

    animation beginning

--- src/containers/bio_text/BioText.jsx
+++ src/containers/bio_text/BioText.jsx
@@ -5,14 +5,14 @@ const BioText = () => {
   return (
     <div className='website__biotext'>
         <link rel="stylesheet" href="css/animate.css"></link>
-        <div className='website__biotext-bio'>
-            Bio
-        </div>
-        <div className='website__biotext-description'>
-            <p>
-                An imaginative Dartmouth College student double majoring in computer science and engineering, specializing in backend development and power electronics. 
-            </p>
-        </div>
+          <div className='website__biotext-bio'>
+              Bio
+          </div>
+          <div className='website__biotext-description'>
+              <p>
+                  An imaginative Dartmouth College student double majoring in computer science and engineering, specializing in backend development and power electronics. 
+              </p>
+          </div>
     </div>
   )
 }
