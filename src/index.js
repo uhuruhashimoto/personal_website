e1bc2182f5ea7d12a96f6a8bda298848a1175228
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-27 12:08:21 -0800

    a comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+/*
+* see https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+*/
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App tab="home" />);
