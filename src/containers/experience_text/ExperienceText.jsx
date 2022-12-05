9fc578b5d24d35987e0d26ab382f785aa6604148
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-12-04 21:44:48 -0800

    achievements edit

--- src/containers/experience_text/ExperienceText.jsx
+++ src/containers/experience_text/ExperienceText.jsx
@@ -13,22 +13,22 @@ const ExperienceText = () => {
                     Professional Experience<br></br><br></br>
                 </p1>
                 <p2>
-                    Ellensburg High School, Nordic Ski Team Assistant Coach. Helped increase youth outdoor activity and community cohesion during the pandemic.<br></br><br></br>
+                    Backend Python Developer -- Ascend Analytics. Contributed to codebase strategy organization and optimized bidding schedule using throughput proxies to meet hardware operating specifications. Summer 2022 - present<br></br><br></br>
                 </p2>
                 <p3>
-                    Hanover High School. XC Assistant Coach. Led Hanover XC to its best season in more than a decade by qualifying for New England Interscholastic Championships.<br></br><br></br>
+                    XC Assistant Coach -- Hanover High School. Led Hanover XC to its best season in more than a decade by qualifying for New England Interscholastic Championships. Fall 2021<br></br><br></br>
                 </p3>
                 <p4>
-                    Ascend Analytics. Backend Python Development. Contributed to codebase strategy organization and optimized bidding schedule using throughput proxies to meet hardware operating specifications.<br></br><br></br>
+                    Nordic Ski Team Assistant Coach -- Ellensburg High School. Helped increase youth outdoor activity and community cohesion during the pandemic. Winter 2020<br></br><br></br>
                 </p4>
                 <p5>
-                    Languages: C, C#, C++, Python, Java, Rust, JavaScript, TypeScript, CSS, Assembly<br></br>
+                    Languages: C, C#, C++, Python, Java, JavaScript, Rust, Assembly <br></br>
                 </p5>
                 <p6>
-                    Embedded Systems: C code, Arduino, VHDL.<br></br>
+                    Frameworks: React, Node.js, Unity<br></br> 
                 </p6>
                 <p7>
-                    Experience: Android app dev, Python backend and ML, C embedded system code, OS experience<br></br>
+                    Embedded Systems: VHDL, Arduino, bare metal C and C++<br></br>
                 </p7>
 
             </div>
