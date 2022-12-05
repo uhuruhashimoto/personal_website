2b73b81719b49095d5fc93e8f0ad68d602f27067
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-12-04 20:49:22 -0800

    project text edit

--- src/containers/projects_text/ProjectsText.jsx
+++ src/containers/projects_text/ProjectsText.jsx
@@ -9,50 +9,64 @@ const ProjectsText = () => {
             Software Development<br></br>
         </p1>
         <p2>
-            AR Gift App on Android. User login, verification, and virtual token system. Unity integration and scripting. Java. 21 Winter<br></br>
+            <h>Meshtastic</h><br></br>
+            Implemented dataflow and backend for mesh network for use in emergency response, including mesh network algorithm optimization and dataflow. TS, C++, Rust. Fall 2022; development ongoing<br></br>
         </p2>
         <p3>
-            Blockchain Playlist Voting System. Song queue selected by blockchain verification of voters. Mining incentivized. Python. 21 Spring<br></br>
+            <h>Yalnix </h><br></br>
+            Wrote 'Nix-style monolithic, preemptive, ring-0-only kernel with double address space to run on Ubuntu (with configured signal handlers). C. Fall 2022<br></br>
         </p3>
         <p4>
-            ML modeling on MNIST dataset; achieved 96% accuracy. Python. 21 Spring<br></br>
+            <h>AR Gift App on Android </h><br></br>
+            User login, verification, and virtual token system. Unity integration and scripting. Java, C#. Winter 2021<br></br>
         </p4>
         <p5>
-            Search Engine. Query parsing, recursive web crawling, and result ranking algorithm. C 21 Summer<br></br>
+            <h>Blockchain Playlist Voting System </h><br></br>
+            Song queue selected by blockchain verification of voters. Mining incentivized. Python. Spring 2021<br></br>
         </p5>
         <p6>
-            Sudoku Solver. Recursive DFS with some branch optimization. C 21 Summer<br></br>
+            <h>Simulated Y86 Architecture </h><br></br>
+            Built 'Y86' X86 simulated architecture from transistor level up. Runs on abbreviated X86 ISA. Includes mmapped terminal and read/write functionality. Run hello world or simple programs (ported to hex)! Assembly. Spring 2022<br></br>
         </p6>
         <p7>
-            Computer Architecture; built 'Y86' X86 simulated architecture from transistor level up. Runs on abbreviated X86 ISA. Includes mmapped terminal and read/write functionality. Run hello world or simple programs (ported to hex)! Spring 2022<br></br>
+            <h>Search Engine </h><br></br>
+            Query parsing, recursive web crawling, and result ranking algorithm. C. Summer 2021<br></br>
         </p7>
         <p8>
-            Operating Systems: wrote 'Yalnix' monolithic, preemptive, ring-0-only kernel with double address space to run on Ubuntu (with configured signal handlers). Fall 2022<br></br>
+            <h>Neural Network </h><br></br>
+            ML modeling on MNIST dataset with 96% accuracy using Jupyter Notebook. Python. Spring 2021<br></br>
         </p8>
-        <p9 className="website__projectstext-description__title2">
-            Embedded Systems Development<br></br>
+        <p9>
+            <h>Sudoku Solver </h><br></br>
+            Recursive DFS with some branch optimization to meet runtime requirements. C. Summer 2021<br></br>
         </p9>
-        <p10>
-            Wordle on FPGA. On-chip resource allocation and state machine functionality for integrated circuit design. 100 MHz clock speed. Runs on full Wordle dictionary. Xilinx Vivido, VHDL. Spring 2022<br></br>
+        <p10 className="website__projectstext-description__title2">
+            Embedded Systems Development<br></br>
         </p10>
         <p11>
-            Meshtastic: implemented dataflow and backend for mesh network for use in emergency response, including mesh network algorithm optimization and dataflow. TS, C++, Rust. 22 Fall; development ongoing<br></br>
+            <h>Wordle on FPGA </h><br></br>
+            On-chip resource allocation and state machine functionality for integrated circuit design. 100 MHz clock speed. Runs on full Wordle dictionary. Xilinx Vivido, VHDL. Spring 2022<br></br>
         </p11>
         <p12>
-            Arduino bare-metal code projects (pwm driver, register signal handlers, network FSM). 2022 Winter Spring<br></br> 
+            <h>Arduino Projects </h><br></br>
+            bare-metal code projects (pwm driver, register signal handlers, network FSM). C. Spring 2022<br></br> 
         </p12>
         <p13 className="website__projectstext-description__title3">
             Power Electronics<br></br>
         </p13>
         <p14>
-            PD Control of Duck Car and accompanying Frequency Domain Analysis. Hardware tested. 21 Fall<br></br>
+            <h>Benchtop AC/DC Converter </h><br></br>
+            High-Power Rectifier, using Discontinuous Indirect Converter and inductor built to specifications. Graduate Power Electronics. Fall 2022<br></br>
         </p14>
         <p15>
-            Breadboard Heart Rate Monitor - electret microphone signal processing, filtering, Class D amplification on breadboards. User tested. 22 Winter<br></br>
+            <h>Duck Car with Feedback Control </h><br></br>
+            PD Control of Duck Car and accompanying Frequency Domain Analysis. Hardware tested. Fall 2021<br></br>
         </p15>
         <p16>
-            High-Power Rectifier, using Discontinuous Indirect Converter and inductor built to specifications. Graduate Power Electronics. 22 Fall<br></br>
+            <h>Breadboard Heart Rate Monitor </h><br></br>
+            Electret microphone signal processing, filtering, Class D amplification on breadboards. User tested. Winter 2022<br></br>
         </p16>
+
     </div>
 </div>
   )
