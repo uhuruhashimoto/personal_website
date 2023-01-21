60983b215ab6b7c696b73663412a81109bf55475
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-01-20 21:51:22 -0500

    solved resume download corruption issue with correct import

--- src/components/infobar/Infobar.jsx
+++ src/components/infobar/Infobar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './infobar.css';
+import resume from '../../assets/Hashimoto_Resume.pdf';
 
 const Infobar = () => {
   return (
     <div className="website__infobar">
         <div className="website__infobar-links">
             <div className="website__infobar-links_container">
-            <p><a href="/src/assets/Hashimoto_Resume.pdf" download="Hashimoto_Resume.pdf">Download Resume</a></p>
+            <p><a href={resume} download="Hashimoto_Resume.pdf">Download Resume</a></p>
             <p><a href="https://github.com/uhuruhashimoto">GitHub</a></p>
             <p><a href="mailto:uhuru.g.hashimoto.23@dartmouth.edu">Contact Me</a></p>
             </div>
